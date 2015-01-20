370cb7da7d468d3baa63606c9140388f6d73f33c
Kevin Whelan <Kevinwhelan2098@gmail.com>
2015-01-20 16:34:51 -0500

    Yikes

--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,35 +1,24 @@
 require_relative '../lib/conway'
-
-
 describe Board do
- it '#creates a game board' do
-   expect(Board.new)
- end
-end
-
-describe '#board_initialize' do
- it 'the board is 80x80' do
-   expect(Board.new.length.count).to eq 80
-   expect(Board.new.width.count).to eq 80
- end
+  it 'creates a board' do
+    expect(Board.new)
+  end
 end
 
 describe '#grid' do
- it 'creates the board' do
-   expect(Board.new.width[0][0]).to eq nil
- end
+  # grid does not take any arguments because its always 80x80
+  it ' expects -grid- creates an 80x80 array inside of array grid using Array.new(80).map { |ele| Array.new(80, true)' do
+    expect(Board.new.grid.count).to eq 80
+  end
 end
+
 
 
 describe Cell do
-
- before(:all) do
-   @cell = Cell.new(2,2)
- end
-
- describe '#live?' do
-   it 'checks to see if the cell is live or dead' do
-     expect(@cell.live?).to eq true
-   end
- end
+  it 'creates a Cell' do
+  @cell = Cell.new
 end
+end
+
+
+
