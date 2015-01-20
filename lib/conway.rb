af95f82236c669b16d3032cee83eade4be224644
Kevin Whelan <Kevinwhelan2098@gmail.com>
2015-01-20 16:34:30 -0500

    Yikes

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,29 +1,12 @@
 class Board
- def initialize
-   @length = length
-   @width = width
- end
-
- def length
-   @length = (0..79).to_a
- end
-
- def width
-   length.map {|ele| ele = (0..79).to_a}
- end
+  def grid
+    Array.new(80)
+  end
 end
+
+
+
 
 class Cell
- def initialize(x, y)
-   @x = x
-   @y = y
-   true
- end
-
- def live?
-   index[x][y]
- end
-
-
-
 end
+# we dont suck lets see it fail
